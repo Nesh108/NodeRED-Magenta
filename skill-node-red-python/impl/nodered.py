0f303942a546664e184b27a76e7a14eac97f751a
Nesh108 <contact@albertovaccari.com>
2020-12-05 11:41:01 +0100

    added intra-communication between tcp server and skill

--- skill-node-red-python/impl/nodered.py
+++ skill-node-red-python/impl/nodered.py
@@ -11,23 +11,28 @@ from skill_sdk import skill, Response, tell
 from skill_sdk.l10n import _
 import time
 import manage
-
+import socket
+import json
 
 @skill.intent_handler('TEAM_36_RED_NODE')
 def handler(input_request: str) -> Response:
     print('computing...')
     request_client = "Magenta7828a"
-   # input_request = "open windows"
 
     print("Connected client: ", manage.current_client)
     print("Input request: ", input_request)
 
-    if manage.current_client == request_client:
-        client_message = input_request
-        while manage.client_response == "":
-            time.sleep(0.1)
-        response = tell(manage.client_response)
-        return response
-    else:
-        response = tell("NodeClient is not connected")
-        return response
+    message = {"message": input_request, "to": request_client}
+
+    try:
+        sock = socket.create_connection(('localhost', 7979))
+        sock.sendall(bytes(json.dumps(message).encode('utf-8')))
+
+        client_response = sock.recv(1024).decode("utf-8").strip('\n')
+        print("received: ", client_response)
+    finally:
+        print("finished receiving")
+        sock.close()
+
+    response = tell(client_response)
+    return response
--- skill-node-red-python/manage.py
+++ skill-node-red-python/manage.py
@@ -3,16 +3,18 @@ from skill_sdk.manage import manage
 import threading
 import socket
 import json
+import time
+import queue
 
 print('starting...')
 
-current_client = "Magenta7828a"
-client_message = "get temperature"
-client_response = "temperature is 16C"
+current_client = ""
+client_message = queue.Queue()
+client_response = ""
 
 
-def start_server(port=65432):
-    global current_client, client_message, client_response
+def start_server(c_msg, port=65432):
+    global current_client, client_response
     print('initializing...')
     # Create a socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,11 +42,15 @@ def start_server(port=65432):
                         data = json.loads(received_data)
                         print("Connected: ", data["id"])
                         current_client = data["id"]
+                        new_socket.send(bytes(json.dumps({"message": "Connected as " + current_client}).encode('utf-8')))
                     if received_data and "{\"response\":" in received_data:
                         client_response = json.loads(received_data)["response"]
-                    elif client_message != "":
-                        new_socket.send(bytes(client_message.encode("utf-8")))
-                        client_message = ""
+                        new_socket.send(bytes(json.dumps({"message": "OK", "to": current_client}).encode('utf-8')))
+                    else:
+                        msg = c_msg.get()
+                        if msg != "":
+                            print("Sending to client: ", msg)
+                            new_socket.send(bytes(msg.encode("utf-8")))
                 except ConnectionResetError:
                     new_socket.close()
                     print("Removing: ", current_client)
@@ -54,8 +60,48 @@ def start_server(port=65432):
         sock.close()
 
 
-server_thread = threading.Thread(target=start_server, name="TCP Server", daemon=True)
+def start_comm_server(c_msg, port=7979):
+    global current_client, client_response
+
+    # Create a socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Ensure that you can restart your server quickly when it terminates
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+    # Set the client socket's TCP "well-known port" number
+    well_known_port = port
+    sock.bind(('', well_known_port))
+
+    # Set the number of clients waiting for connection that can be queued
+    sock.listen(5)
+
+    # loop waiting for connections (terminate with Ctrl-C)
+    try:
+        while 1:
+            new_socket, address = sock.accept()
+            print("Comm: Connected from", address)
+            # loop serving the new client
+            while 1:
+                try:
+                    received_data = new_socket.recv(1024).decode("utf-8").strip('\n')
+                    if received_data and "{\"message\":" in received_data:
+                        print("Comm: Message: ", received_data)
+                        c_msg.put(received_data)
+
+                        while client_response == "":
+                            time.sleep(0.1)
+                        new_socket.send(bytes(client_response.encode('utf-8')))
+                except ConnectionResetError:
+                    new_socket.close()
+                    print("Comm: Disconnected from", address)
+    finally:
+        sock.close()
+
+
+server_thread = threading.Thread(target=start_server, name="TCP Server", daemon=True, args=(client_message, 65432))
 server_thread.start()
 print('started')
-
+comm_thread = threading.Thread(target=start_comm_server, name="Comm Server", daemon=True, args=(client_message, 7979))
+comm_thread.start()
 manage()
