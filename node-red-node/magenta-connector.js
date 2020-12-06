29df246ea40f91f343630be5028cb3d9809d87ae
Reinhard Henning <reinhard.henning@gmx.de>
2020-12-06 22:29:29 +0000

    working - added status

--- node-red-node/magenta-connector.js
+++ node-red-node/magenta-connector.js
@@ -1,11 +1,16 @@
-
-
 module.exports = function(RED) {
 	
+	/////////////////////////////////////////////////////////////
+	//
+	//   
+	//
+	/////////////////////////////////////////////////////////////
+	// when the connection has established 	
+	//     send {"id": "<DEVICE_NAME>"} 
+	// send every message from the server to the output
+	// send every input message to the server 
+	/////////////////////////////////////////////////////////////
 	
-	// {"id": "<DEVICE_NAME>"} and then wait for messages from the server.	
-	// {"message": input_request, "to": request_client}`
-
     function MagentaConnector(config) {
 		var net = require('net'); //https://nodejs.org/api/net.html
 		let socket = new net.Socket()
@@ -14,7 +19,6 @@ module.exports = function(RED) {
 		this.port = config.port * 1;
 		this.device = config.device;
 		
-
         RED.nodes.createNode(this,config);
 
 		msg = {"topic":this.topic, "payload":""};
@@ -33,9 +37,12 @@ module.exports = function(RED) {
 		};
 		
 		socket.setEncoding('utf8');
-	
+		node.status({fill:"white",shape:"dot",text:""});
+
+
 		// connect to Magenta Skill Server
 		function Connect() {
+			node.status({fill:"yellow",shape:"dot",text:"connecting"});
 			if (node.debug === 'all') node.warn('Data received from ${socket.remoteAddress}:${socket.remotePort}');
 			node.log("connecting to " + options.host + " port " + options.port);
 			socket.connect(options);
@@ -54,13 +61,15 @@ module.exports = function(RED) {
 		}
 
 		socket.on('error', function(err) {
-			node.log('socket error ' + err.code);
+			node.log('socket error ' + err.code);			
+			node.status({fill:"red",shape:"dot",text:"disconnected"});
 			Reconnect();
 		});
 
 		socket.on('connect', function() {
 			node.log('connection established');			
 			SendHelloServer();
+			node.status({fill:"green",shape:"dot",text:"connected"});
 		});
 		
 		socket.on('data', function(data) {
@@ -85,7 +94,8 @@ module.exports = function(RED) {
         });
 
 		node.on("close",function() {
-			node.log("node close");		
+			node.log("node close");	
+			node.status({fill:"red",shape:"dot",text:"disconnected"});			
 		})
 			
 		Connect();			
